9522e80345b54209d4bed6c8a1366b8ccd297c1b
orz <306421565@qq.com>
2024-06-28 09:46:13 +0800

    using cargo fmt

--- raytracer/src/ray.rs
+++ raytracer/src/ray.rs
@@ -1,16 +1,22 @@
 use crate::vec3::Vector;
 
-pub struct Ray{
-    pub origin:Vector,
-    pub direction:Vector,
+pub struct Ray {
+    pub origin: Vector,
+    pub direction: Vector,
 }
 
-impl Ray{
-    pub fn at(&self,t:f64)->Vector{
-        Vector{
-            x:self.origin.x+t*self.direction.x,
-            y:self.origin.y+t*self.direction.y,
-            z:self.origin.z+t*self.direction.z,
+impl Ray {
+    pub fn new(a: Vector, b: Vector) -> Self {
+        Self {
+            origin: a,
+            direction: b,
+        }
+    }
+    pub fn at(&self, t: f64) -> Vector {
+        Vector {
+            x: self.origin.x + t * self.direction.x,
+            y: self.origin.y + t * self.direction.y,
+            z: self.origin.z + t * self.direction.z,
         }
     }
 }
--- raytracer/src/vec3.rs
+++ raytracer/src/vec3.rs
@@ -1,70 +1,81 @@
-pub struct Vector{
-    pub x:f64,
-    pub y:f64,
-    pub z:f64,
+use std::ops::{Add, Div, Mul, Sub};
+
+#[derive(Copy, Clone)]
+pub struct Vector {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
 }
 
-impl Vector{
-    pub fn new(a:f64,b:f64,c:f64)->Self{
-        Self{
-            x:a,
-            y:b,
-            z:c,
+impl Vector {
+    pub fn new(a: f64, b: f64, c: f64) -> Self {
+        Self { x: a, y: b, z: c }
+    }
+    pub fn length(&self) -> f64 {
+        f64::sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
+    }
+    pub fn length_square(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+    pub fn dot(&self, other: &Vector) -> f64 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+    pub fn cross(&self, other: &Vector) -> Vector {
+        Vector {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
         }
     }
-    pub fn length(&self)->f64{
-        f64::sqrt(self.x*self.x+self.y*self.y+self.z*self.z)
-    }
-    pub fn length_square(&self)->f64{
-        self.x*self.x+self.y*self.y+self.z*self.z
-    }
-    pub fn dot(&self,other:&Vector)->f64{
-        self.x*other.x+self.y*other.y+self.z*other.z
-    }
-    pub fn cross(&self,other:&Vector)->Vector{
-        Vector{
-            x:self.y*other.z-self.z*other.y,
-            y:self.z*other.x-self.x*other.z,
-            z:self.x*other.y-self.y*other.x,
+    pub fn unit(&self) -> Vector {
+        let len: f64 = self.length();
+        Vector {
+            x: self.x / len,
+            y: self.y / len,
+            z: self.z / len,
         }
     }
-    pub fn add(&self,other:&Vector)->Vector{
-        Vector{
-            x:self.x+other.x,
-            y:self.y+other.y,
-            z:self.z+other.z,
+    pub fn print(&self) {
+        println!("x:{} y:{} z:{}", self.x, self.y, self.z);
+    }
+}
+impl Add for Vector {
+    type Output = Self;
+    fn add(self, other: Self) -> Self::Output {
+        Vector {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
         }
     }
-    pub fn sub(&self,other:&Vector)->Vector{
-        Vector{
-            x:self.x-other.x,
-            y:self.y-other.y,
-            z:self.z-other.z,
+}
+impl Sub for Vector {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self::Output {
+        Vector {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
         }
     }
-    pub fn mul(&self,other:&f64)->Vector{
-        Vector{
-            x:self.x*other,
-            y:self.y*other,
-            z:self.z*other,
+}
+impl Mul<f64> for Vector {
+    type Output = Self;
+    fn mul(self, other: f64) -> Self::Output {
+        Vector {
+            x: self.x * other,
+            y: self.y * other,
+            z: self.z * other,
         }
     }
-    pub fn div(&self,other:&f64)->Vector{
-        Vector{
-            x:self.x/other,
-            y:self.y/other,
-            z:self.z/other,
+}
+impl Div<f64> for Vector {
+    type Output = Self;
+    fn div(self, other: f64) -> Self::Output {
+        Vector {
+            x: self.x / other,
+            y: self.y / other,
+            z: self.z / other,
         }
-    }
-    pub fn unit(&self)->Vector{
-        let len:f64=self.length();
-        Vector{
-            x:self.x/len,
-            y:self.y/len,
-            z:self.z/len,
-        }
-    }
-    pub fn print(&self){
-        println!("x:{} y:{} z:{}",self.x,self.y,self.z);
     }
 }
