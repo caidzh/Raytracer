c27e3c21202379db3d2e785f9e80f3f493481cdf
orz <306421565@qq.com>
2024-06-28 09:52:42 +0800

    using cargo fmt

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -5,15 +5,15 @@ use std::f64;
 use std::{fs::File, process::exit};
 pub mod ray;
 pub mod vec3;
-use crate::vec3::Vector;
 use crate::ray::Ray;
+use crate::vec3::Vector;
 
-fn ray_color(r:&Ray)->Vector{
-    let unit_direction:Vector=r.direction.unit();
-    let a=0.5*(unit_direction.y+1.0);
-    let white:Vector=Vector::new(1.0,1.0,1.0);
-    let blue:Vector=Vector::new(0.5,0.7,1.0);
-    (white*(1.0-a)+blue*a)*255.99
+fn ray_color(r: &Ray) -> Vector {
+    let unit_direction: Vector = r.direction.unit();
+    let a = 0.5 * (unit_direction.y + 1.0);
+    let white: Vector = Vector::new(1.0, 1.0, 1.0);
+    let blue: Vector = Vector::new(0.5, 0.7, 1.0);
+    (white * (1.0 - a) + blue * a) * 255.99
 }
 
 fn main() {
@@ -21,10 +21,10 @@ fn main() {
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
 
-    let aspect_ratio:f64=16.0/9.0;
-    let image_width:u32=400;
-    let image_height=(image_width as f64/aspect_ratio).round() as u32;
-    let image_height=if image_height<1 {1} else {image_height};
+    let aspect_ratio: f64 = 16.0 / 9.0;
+    let image_width: u32 = 400;
+    let image_height = (image_width as f64 / aspect_ratio).round() as u32;
+    let image_height = if image_height < 1 { 1 } else { image_height };
     let quality = 100;
     let mut img: RgbImage = ImageBuffer::new(image_width, image_height);
 
@@ -34,28 +34,36 @@ fn main() {
         ProgressBar::new((image_height * image_width) as u64)
     };
 
-    let focal_length:f64=1.0;
-    let viewport_height:f64=2.0;
-    let viewport_width:f64=viewport_height*(image_width as f64/(image_height as f64));
-    let camera_center:Vector=Vector::new(0.0,0.0,0.0);
+    let focal_length: f64 = 1.0;
+    let viewport_height: f64 = 2.0;
+    let viewport_width: f64 = viewport_height * (image_width as f64 / (image_height as f64));
+    let camera_center: Vector = Vector::new(0.0, 0.0, 0.0);
 
-    let viewport_u:Vector=Vector::new(viewport_width,0.0,0.0);
-    let viewport_v:Vector=Vector::new(0.0,-viewport_height,0.0);
+    let viewport_u: Vector = Vector::new(viewport_width, 0.0, 0.0);
+    let viewport_v: Vector = Vector::new(0.0, -viewport_height, 0.0);
 
-    let pixel_delta_u:Vector=viewport_u/(image_width as f64);
-    let pixel_delta_v:Vector=viewport_v/(image_height as f64);
+    let pixel_delta_u: Vector = viewport_u / (image_width as f64);
+    let pixel_delta_v: Vector = viewport_v / (image_height as f64);
 
-    let viewport_upper_left=camera_center-Vector::new(0.0,0.0,focal_length)-viewport_u.clone()/2.0-viewport_v.clone()/2.0;
-    let pixel00_loc=viewport_upper_left+(pixel_delta_u+pixel_delta_v)*0.5;
+    let viewport_upper_left = camera_center
+        - Vector::new(0.0, 0.0, focal_length)
+        - viewport_u.clone() / 2.0
+        - viewport_v.clone() / 2.0;
+    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
 
-    for j in (0..image_height).rev(){
-        for i in 0..image_width{
-            let pixel=img.get_pixel_mut(i,j);
-            let pixel_center=pixel00_loc+(pixel_delta_u*(i as f64))+(pixel_delta_v*(j as f64));
-            let ray_direction=pixel_center-camera_center;
-            let r:Ray=Ray::new(camera_center,ray_direction);
-            let pixel_color:Vector=ray_color(&r);
-            *pixel = image::Rgb([pixel_color.x as u8, pixel_color.y as u8, pixel_color.z as u8]);
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let pixel = img.get_pixel_mut(i, j);
+            let pixel_center =
+                pixel00_loc + (pixel_delta_u * (i as f64)) + (pixel_delta_v * (j as f64));
+            let ray_direction = pixel_center - camera_center;
+            let r: Ray = Ray::new(camera_center, ray_direction);
+            let pixel_color: Vector = ray_color(&r);
+            *pixel = image::Rgb([
+                pixel_color.x as u8,
+                pixel_color.y as u8,
+                pixel_color.z as u8,
+            ]);
         }
         progress.inc(1);
     }
